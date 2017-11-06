f229fb04131c2b7783d105248e487d48cb9a54fa
Marko Koron <koron.marko@gmail.com>
2017-11-06 20:11:35 +0100

    Created md cards, fadein effect

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(function() {
   
-
+    $('#test li').fadeIn('4000');
     $('#search').bind('keypress', function(e){
         if (e.keyCode == 13) {
             let searchTerm = $('#search').val();
@@ -11,15 +11,22 @@ $(function() {
                 dataType: 'jsonp',
                 success: function(data){
                     $('.search-box').addClass('fixed');
+                    $('#results').html('');
+                    var fadeDelay = 800;
+                    var leftSlide = -100;
                     for (let i = 0; i < data[1].length; i++) {
                         let title = data[1][i];
                         let excerpt = data[2][i];
                         let url = data[3][i];
-                        let article = `<li><h3>${title}</h3>
-                                            <p>${excerpt}</p>
-                                            <a href="${url}" target="blank">Go to</a>
+                        let article = `<li style='left: ${leftSlide}px'>
+                                            <a href="${url}" target="blank"></a>
+                                                <h3>${title}</h3>
+                                                <p>${excerpt}</p>
                                         </li>`;
-                        $('#results').append(article).fadeIn();
+                        $('#results').append(article);
+                        $('#results li').animate({left: 0, opacity: 1}, fadeDelay);
+                        fadeDelay += 50;
+                        leftSlide -= 20;
                     }
                 },
                 error: function(err){
