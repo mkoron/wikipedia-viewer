7baa4faee6e680e0c72f84df32d58c622dde9892
Marko Koron <koron.marko@gmail.com>
2017-11-06 20:37:36 +0100

    Effect changed to slideDown

--- scripts.js
+++ scripts.js
@@ -13,20 +13,19 @@ $(function() {
                     $('.search-box').addClass('fixed');
                     $('#results').html('');
                     var fadeDelay = 800;
-                    var leftSlide = -100;
+    
                     for (let i = 0; i < data[1].length; i++) {
                         let title = data[1][i];
                         let excerpt = data[2][i];
                         let url = data[3][i];
-                        let article = `<li style='left: ${leftSlide}px'>
+                        let article = `<li>
                                             <a href="${url}" target="blank"></a>
                                                 <h3>${title}</h3>
                                                 <p>${excerpt}</p>
                                         </li>`;
                         $('#results').append(article);
-                        $('#results li').animate({left: 0, opacity: 1}, fadeDelay);
+                        $('#results li').slideDown(fadeDelay).animate({opacity: 1});
                         fadeDelay += 50;
-                        leftSlide -= 20;
                     }
                 },
                 error: function(err){
